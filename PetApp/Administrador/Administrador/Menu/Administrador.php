baddb71f4ffe39b87037dcb47c5bf4956e1fd9c0
Sebasttian28 <43865001+Sebasttian28@users.noreply.github.com>
2018-10-11 07:54:40 -0500

    Add files via upload

---
+++ PetApp/Administrador/Administrador/Menu/Administrador.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="Estilo.css">
+	<title>PetApp</title>
+</head>
+<body>
+	<header id="encabezado">
+		<h1>Administrador</h1>
+		<img src="Imagenes/Admin2.png" id="Avatar" >
+	</header>
+	<div id="pricing" class="container-fluid">
+  <div class="text-center">
+
+    <br><h2>GESTIONAR</h2><br><br>
+    <br>
+  </div>
+
+    <center>
+
+ <div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center" id="divadopcion">
+        <div class="panel-heading">
+          <h1>Adopcion</h1>
+        </div>
+        <div class="panel-body">
+          <input type="image" name="Adopcion" src="logos/Adopcion.png" class="img-responsive slideanim" id="logoimagen">
+        </div>
+      </div>      
+    </div>
+    
+    <div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center" id="divanimal">
+        <div class="panel-heading">
+          <h1>Animales</h1>
+        </div>
+        <div class="panel-body">
+          <input type="image" name="Animales" src="logos/Animales.png" class="img-responsive slideanim" id="logoimagen">
+        </div>
+      </div>      
+    </div>
+
+<div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center" id="divciudadano">
+        <div class="panel-heading">
+          <h1>Ciudadano</h1>
+        </div>
+        <div class="panel-body">
+          <input type="image" name="Eventos" src="logos/Ciudadano.png" class="img-responsive slideanim" id="logoimagen">
+        </div>
+      </div>      
+    </div> 
+
+<div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center" id="divcoment">
+        <div class="panel-heading">
+          <h1>Comentarios</h1>
+        </div>
+        <div class="panel-body">
+          <input type="image" name="Comentarios" src="logos/Comentarios.png" class="img-responsive slideanim" id="logoimagen">
+        </div>
+      </div>      
+    </div>
+
+<div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center" id="divdenuncias">
+        <div class="panel-heading">
+          <h1>Denuncias</h1>
+        </div>
+        <div class="panel-body">
+          <input type="image" name="Denuncias" src="logos/Denuncias.png" class="img-responsive slideanim" id="logoimagen">
+        </div>
+      </div>      
+    </div>
+
+<div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center" id="diveventos">
+        <div class="panel-heading">
+          <h1>Eventos</h1>
+        </div>
+        <div class="panel-body">
+          <input type="image" name="Eventos" src="logos/Eventos.png" class="img-responsive slideanim" id="logoimagen">
+        </div>
+      </div>      
+    </div> 
+
+    <div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center" id="divfuncionario">
+        <div class="panel-heading">
+          <h1>Funcionario</h1>
+        </div>
+        <div class="panel-body">
+          <input type="image" name="Adopcion" src="logos/Funcionario.png" class="img-responsive slideanim" id="logoimagen">
+        </div>
+      </div>      
+    </div>
+
+<div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center" id="divmascotas">
+        <div class="panel-heading">
+          <h1>Mascotas</h1>
+        </div>
+        <div class="panel-body">
+          <input type="image" name="Mascotas" src="logos/Mascotas.png" class="img-responsive slideanim" id="logoimagen">
+        </div>
+      </div>      
+    </div>
+
+<div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center" id="divpostulacion">
+        <div class="panel-heading">
+          <h1>Postulacion</h1>
+        </div>
+        <div class="panel-body">
+          <input type="image" name="Eventos" src="logos/Postulacion.png" class="img-responsive slideanim" id="logoimagen">
+        </div>
+      </div>      
+    </div>  
+
+<div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center" id="divseguimiento"> 
+        <div class="panel-heading">
+          <h1>Seguimiento</h1>
+        </div>
+        <div class="panel-body">
+          <input type="image" name="Eventos" src="logos/Seguimiento.png" class="img-responsive slideanim" id="logoimagen">
+        </div>
+      </div>      
+    </div> 
+
+<div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center" id="divusuario">
+        <div class="panel-heading">
+          <h1>Usuario</h1>
+        </div>
+        <div class="panel-body">
+          <input type="image" name="Veterinaria" src="logos/Usuario.png"  class="img-responsive slideanim" id="logoimagen">
+        </div>
+      </div>      
+    </div>
+
+<div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center" id="divvet">
+        <div class="panel-heading">
+          <h1>Veterinaria</h1>
+        </div>
+        <div class="panel-body">
+          <input type="image" name="Veterinaria" src="logos/Veterinaria.png"  class="img-responsive slideanim" id="logoimagen">
+        </div>
+      </div>      
+    </div> 
+  </center>
+  
+</div>
+
+</body>
+</html>
