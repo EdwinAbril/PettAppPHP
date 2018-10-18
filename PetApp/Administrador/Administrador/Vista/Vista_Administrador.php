660d053361cf392873cdc2daacc5d18cbb9395a5
EdwinAbril <43882730+EdwinAbril@users.noreply.github.com>
2018-10-18 12:26:01 -0500

    Eliminar El Gran ERROR 2.0

--- PetApp/Administrador/Administrador/Vista/Vista_Administrador.php
+++ PetApp/Administrador/Administrador/Vista/Vista_Administrador.php
@@ -3,7 +3,6 @@
 <head>
 	<title>Administrador</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="Estilo.css">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
