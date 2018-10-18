6005e3a520042298b4fc52043d8fb75b923b09e5
EdwinAbril <43882730+EdwinAbril@users.noreply.github.com>
2018-10-18 10:00:32 -0500

    Corrigiendo Errores del Funci XD XD

--- PetApp/Administrador/Funcionario/Modelo/Modelo_Funcionario.php
+++ PetApp/Administrador/Funcionario/Modelo/Modelo_Funcionario.php
@@ -25,7 +25,7 @@ class MetodoFuncionario{
 	$conexion= $conectar->conexion();
 	$sql="call Modificar_Funcionario(?,?,?,?,?)";
 	$consulta=$conexion->prepare($sql);
-	$consulta->bind_param('ssssi',$cedula_p,$telefono_p,$nombre_p,$correo_p,$clave_p);
+	$consulta->bind_param('sssss',$cedula_p,$telefono_p,$nombre_p,$correo_p,$clave_p);
 	$cedula_p=$cedula;
 	$telefono_p=$telefono;
 	$nombre_p=$nombre;
