63c2561089ce6c4aff329181c23723cfc8e81475
Sebasttian28 <43865001+Sebasttian28@users.noreply.github.com>
2018-10-22 11:53:26 -0500

    1

--- PetApp/Conexion.php
+++ PetApp/Conexion.php
@@ -2,15 +2,7 @@
 class conectarproyecto{
 private $servidor="localhost";
 private $usuario="root";
-<<<<<<< HEAD
-<<<<<<< HEAD
 private $pass="0000";
-=======
-private $pass="16072011e";
->>>>>>> 35e7741cd69f23285607747e5dc556d54d4abd28
-=======
-private $pass="16072011e";
->>>>>>> 35e7741cd69f23285607747e5dc556d54d4abd28
 private $bd="proyecto";
 
 public function conexion(){
