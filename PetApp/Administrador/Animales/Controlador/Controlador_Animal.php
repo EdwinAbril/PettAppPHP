02e37de8fccad51e06799b488955ebd993e33bc1
Camilo <cristihancamiloquinterovega@gmail.com>
2018-10-29 07:52:11 -0600

    controlador animal animal

--- PetApp/Administrador/Animales/Controlador/Controlador_Animal.php
+++ PetApp/Administrador/Animales/Controlador/Controlador_Animal.php
@@ -15,7 +15,11 @@ function insertar(){
 	if ($nombre!=null && $tamaño<=1000000) {
         $hoy=date("d_m_y");
         $nombre=$hoy.$nombre;
+
         $carpeta_destino=$_SERVER['DOCUMENT_ROOT'].'/PettAppPHP/PetApp/Animales_fotos/';
+
+        $carpeta_destino=$_SERVER['DOCUMENT_ROOT'].'/GitHub/PettAppPHP/PetApp/Animales_fotos/';
+
         move_uploaded_file($_FILES['imagen']['tmp_name'],$carpeta_destino.$nombre);
     }
 	require_once("../Modelo/Modelo_Animal.php");
