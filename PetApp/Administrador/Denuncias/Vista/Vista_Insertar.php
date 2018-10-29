f9d81f4f4efd42089dc9de20a0b5d24964b4e9b0
EdwinAbril <43882730+EdwinAbril@users.noreply.github.com>
2018-10-29 08:13:52 -0500

    Update PetApp/Administrador/Denuncias/Vista/Vista_Insertar.php

--- PetApp/Administrador/Denuncias/Vista/Vista_Insertar.php
+++ PetApp/Administrador/Denuncias/Vista/Vista_Insertar.php
@@ -12,17 +12,26 @@
 </head>
 <body>
         <form action="../Vista/Vista_Denuncias.php">
+
     <input type="submit" name="volver" value="Volver" class="btn btn-outline-dark" id="vol">
     </form>
+  <input type="submit" name="volver" value="Volver" class="btn btn-outline-dark" id="vol">
+</form>
+<center>
+    <div id="insertar" >
 	<h1>Insertar Denuncia</h1>
  <form action="../Controlador/Controlador_Insertar.php"" method="Post"  enctype="multipart/form-data">
- 	<p><label>Fecha De la Denuncia</label></p><input type="date" name="fecha">
-    	<p><label>Tipo de Denuncia</label></p><input type="text" name="tipo">
-    	<p><label>Cedula del Denunciante</label></p><input type="number" name="cedula">
-    	<p><label>Denuncia</label></p><input type="text" name="denuncia">
-    	<p><label>Foto</label></p><input type="file" name="imagen" accept="image/png,image/jpeg,image/jpg"></label>
-    	<input type="submit" name="consulta" value="Realizar Denuncia">
+ 	<p><label>Fecha De la Denuncia</label></p><input type="date" name="fecha" required="">
+    	<p><label>Tipo de Denuncia</label></p><input type="text" name="tipo" required="">
+    	<p><label>Cedula del Denunciante</label></p><input type="number" name="cedula" required="">
+    	<p><label>Denuncia</label></p><input type="text" name="denuncia" required="">
+    	<p><label>Foto</label></p><center><input type="file" name="imagen" id="quitar" accept="image/png,image/jpeg,image/jpg"></center></label>
+           </br>
+    </br>
+    	<input type="submit" name="consulta" value="Realizar Denuncia" class="btn btn-primary">
     </form>
+</div>
+</center>
     <?php
 if(isset($_POST['consulta']))
 {
