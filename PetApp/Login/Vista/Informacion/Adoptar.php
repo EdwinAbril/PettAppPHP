e4897d269dc096ece8231a64dc8de344e103e70d
Sebasttian28 <43865001+Sebasttian28@users.noreply.github.com>
2018-11-19 12:09:04 -0500

    Informacion

--- PetApp/Login/Vista/Informacion/Adoptar.php
+++ PetApp/Login/Vista/Informacion/Adoptar.php
@@ -9,18 +9,49 @@
 	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/Footer-white.css">
 	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-	<script src="main.js"></script>
-  	<script src="js/jquery-3.3.1.min.js" ></script>
-  	<script src="js/main.js" ></script>
 
 	<title>Adoptar</title>
 </head>
 
+<div id="contenedor">
 <header><img src="Imagen.jpg" id="imagen"></header> 
 <body>
 
 <h1 id="Texto">Cómo Adoptar?</h1>
+
+
+<img src="Imagen2.jpeg" class="item" id="imagen2">
+	
+	<article id="Informacion">
+
+		<p id="parrafo1">
+			En este apartado usted encontrará los animales disponibles, podrá ver unas filas desplegables con los nombres de los animales, al desplegar dando clic en el nombre encontrará las características del animar además de la opción de adoptar, allí lo dirigirá al formulario de postulación, dónde usted deberá ingresar sus datos personales, para aplicar al proceso de adopción, después de esto la alcaldía analizará sus datos y seleccionará al ciudadano que considere más apropiado para adoptar el animal, usted será informado acerca de la decisión que tome la alcaldía a través de un mensaje, si es seleccionado podra seguir con el proceso.
+		</p>
+	</article>
+
 </body>
+
+<footer id="myFooter">
+        <div class="container">
+            <ul>
+                <li><a href="#" >Alcaldia de Mosquera</a></li>
+                <li><a href="#">Contactenos</a></li>
+                <li><a href="#">Reviews</a></li>
+                <li><a href="#">Terms of service</a></li>
+            </ul>
+        <p class="footer-copyright">© 2018 Copyright</p>
+        </div>
+        <div class="footer-social">
+            <a href="#" class="social-icons"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="social-icons"><i class="fa fa-google-plus"></i></a>
+            <a href="#" class="social-icons"><i class="fa fa-twitter"></i></a>
+        </div>
+</footer>
+
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+</script>
+
+</div>
 
 </html>
 
